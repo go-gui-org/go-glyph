3f1b0d0cab1771bb2baa7219edeca188dc5bba6b
mike-ward <mike@mike-ward.net>
2026-07-08 11:28:08 -0500

    fix: remove unused get/put, fix remaining gofmt parens in build tags

--- cache.go
+++ cache.go
@@ -9,9 +9,3 @@ type metricsCache struct{}
 func newMetricsCache(capacity int) metricsCache {
 	return metricsCache{}
 }
-
-func (c *metricsCache) get(key uint64) (struct{ Ascent, Descent, LineGap int }, bool) {
-	return struct{ Ascent, Descent, LineGap int }{}, false
-}
-
-func (c *metricsCache) put(key uint64, entry struct{ Ascent, Descent, LineGap int }) {}
--- renderer_ft.go
+++ renderer_ft.go
@@ -1,4 +1,4 @@
-//go:build android || (linux)
+//go:build android || linux
 
 package glyph
 
